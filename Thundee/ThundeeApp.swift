16d3b07f0f834a33169b5ca27ab2dd842a0ff74a
Michał Nierebiński <contact@caninto.space>
2022-06-07 18:23:24 +0200

    Make project great again

---
+++ Thundee/ThundeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThundeeApp.swift
+//  Thundee
+//
+//  Created by Michal Nierebinski on 07/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct ThundeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
